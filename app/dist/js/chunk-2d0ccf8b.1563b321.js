b73329fd4f8954e24fa9a879b8b5da4f5362a167
Rastislav Madera <maderarasto@gmail.com>
2020-09-13 01:43:15 +0200

    Adds a demo to the public folder.

---
+++ app/dist/js/chunk-2d0ccf8b.1563b321.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ccf8b"],{"4fcf":function(n,e,s){"use strict";s.r(e);var c=function(){var n=this,e=n.$createElement,s=n._self._c||e;return s("div",{staticClass:"liked-songs"})},t=[],l={name:"LikedSongs"},a=l,i=s("2877"),u=Object(i["a"])(a,c,t,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0ccf8b.1563b321.js.map
