b7b58f40bc624dad4b042b2e17995faae390a7ec
Rastislav Madera <maderarasto@gmail.com>
2020-09-21 22:41:28 +0200

    Moves a playlist header code into its own component named PlaylistHeader.

---
+++ app/src/components/PlaylistHeader.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="playlist-header">
+    <div class="content">
+      <div class="playlist-image">
+        <img v-if="image" :src="image.url" />
+      </div>
+      <div class="playlist-info">
+        <span class="playlist-type">Playlist</span>
+        <h2 class="playlist-title">{{ title }}</h2>
+        <p v-if="description" class="playlist-description">{{ description }}</p>
+        <ul class="additional-info">
+          <li class="playlist-author">{{ getAuthorName() }}</li>
+          <li v-if="followers" class="playlist-likes">{{ $utils.formatNumber(followers) }} likes</li>
+          <li class="playlist-length">{{ length }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlaylistHeader',
+  
+  props: {
+    playlist: Object
+  },
+
+  computed: {
+    image() {
+      return this.playlist ? this.playlist.images[0] : '';
+    },
+
+    title() {
+      return this.playlist ? this.playlist.name : '';
+    },
+
+    description() {
+      return this.playlist ? this.playlist.description : '';
+    },
+
+    author() {
+      return this.playlist ? this.playlist.owner : '';
+    },
+
+    followers() {
+      return this.playlist ? this.playlist.followers.total : 0;
+    },
+
+    duration() {
+      return this.playlist ? this.playlist.tracks.items.reduce((initial, item) => {
+        initial += item.track.duration_ms;
+
+        return initial;
+      }, 0) : 0;
+    },
+
+    length() {
+      const duration = this.$utils.parseTime(this.duration);
+      
+      const hours = duration.hours ? `${duration.hours} hr` : ''
+      const minutes = duration.minutes ? `${duration.minutes} min` : '';
+      const seconds = duration.seconds ? `${duration.seconds} sec`  : '';
+
+      return hours ? `${hours} ${minutes}` : minutes ? `${minutes} ${seconds}` : seconds;
+    },
+
+    owner() {
+      return this.author ? this.author.id === this.user.id : false;
+    }
+  },
+
+  methods: {
+    getAuthorName() {
+      return this.author ? this.author.display_name : '';
+    }
+  }
+}
+</script>
+
+<style>
+.playlist-header{
+  position: relative;
+
+  width: 100%;
+  height: 400px;
+  background: linear-gradient(#606060, #202020);
+}
+
+.playlist-header .content {
+  position: absolute;
+  left: 40px;
+  bottom: 30px;
+
+  height: 225px;
+}
+
+.playlist-header .content .playlist-image {
+  display: block;
+  width: 225px;
+  float: left;
+}
+
+.playlist-header .content .playlist-image > img{
+  width: 100%;
+  height: 100%;
+}
+
+.playlist-header .content .playlist-info {
+  display: flex;
+  min-width: 600px;
+  height: 225px;
+  margin-left: 40px;
+  float: right;
+
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+
+  color: #8d8d8d;
+}
+
+.playlist-info .additional-info {
+  padding: 10px 0 0;
+}
+
+.playlist-info .additional-info li {
+  display: inline;
+  padding-left: 10px;
+  list-style: none;
+}
+
+.playlist-info .additional-info .playlist-author {
+  padding-left: 0;
+  font-weight: bold;
+  color: white;
+}
+
+.playlist-info .additional-info li::before {
+  content: '\2022';
+  margin-right: 10px;
+}
+
+.playlist-info .additional-info .playlist-author::before {
+  content: '';
+  margin-right: 0;
+}
+
+.playlist-info .playlist-description {
+  margin: 10px 0 0;
+}
+
+.playlist-info .playlist-title {
+  margin-bottom: 10px;
+  font-size: 48pt;
+  color: white;
+}
+
+.playlist-info .playlist-type {
+  margin-bottom: 30px;
+
+  font-weight: bold;
+  text-transform: uppercase;
+  color: white;
+}
+</style>
--- app/src/components/containers/Playlist.vue
+++ app/src/components/containers/Playlist.vue
@@ -1,22 +1,6 @@
 <template>
   <div class="playlist">
-    <div class="playlist-header">
-      <div class="content">
-        <div class="playlist-image">
-          <img v-if="image" :src="image.url" />
-        </div>
-        <div class="playlist-info">
-          <span class="playlist-type">Playlist</span>
-          <h2 class="playlist-title">{{ title }}</h2>
-          <p v-if="description" class="playlist-description">{{ description }}</p>
-          <ul class="additional-info">
-            <li class="playlist-author">{{ author.display_name }}</li>
-            <li v-if="followers" class="playlist-likes">{{ $utils.formatNumber(followers) }} likes</li>
-            <li class="playlist-length">{{ length }}</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <PlaylistHeader :playlist="playlist" />
     <div class="playlist-body">
 
     </div>
@@ -24,8 +8,13 @@
 </template>
 
 <script>
+import PlaylistHeader from '@/components/PlaylistHeader';
+
 export default {
   name: 'Playlist',
+  components: {
+    PlaylistHeader
+  },
 
   data() {
     return {
@@ -36,48 +25,6 @@ export default {
   computed: {
     user() {
       return this.$store.getters.user;
-    },
-
-    image() {
-      return this.playlist ? this.playlist.images[0] : '';
-    },
-
-    title() {
-      return this.playlist ? this.playlist.name : '';
-    },
-
-    description() {
-      return this.playlist ? this.playlist.description : '';
-    },
-
-    author() {
-      return this.playlist ? this.playlist.owner : '';
-    },
-
-    followers() {
-      return this.playlist ? this.playlist.followers.total : 0;
-    },
-
-    duration() {
-      return this.playlist ? this.playlist.tracks.items.reduce((initial, item) => {
-        initial += item.track.duration_ms;
-
-        return initial;
-      }, 0) : 0;
-    },
-
-    length() {
-      const duration = this.$utils.parseTime(this.duration);
-      
-      const hours = duration.hours ? `${duration.hours} hr` : ''
-      const minutes = duration.minutes ? `${duration.minutes} min` : '';
-      const seconds = duration.seconds ? `${duration.seconds} sec`  : '';
-
-      return hours ? `${hours} ${minutes}` : minutes ? `${minutes} ${seconds}` : seconds;
-    },
-
-    owner() {
-      return this.author.id === this.user.id;
     }
   },
 
@@ -95,90 +42,5 @@ export default {
   height: 100%;
 
   flex-direction: column;
-}
-
-.playlist-header{
-  position: relative;
-
-  width: 100%;
-  height: 400px;
-  background: linear-gradient(#606060, #202020);
-}
-
-.playlist-header .content {
-  position: absolute;
-  left: 40px;
-  bottom: 30px;
-
-  height: 225px;
-}
-
-.playlist-header .content .playlist-image {
-  display: block;
-  width: 225px;
-  float: left;
-}
-
-.playlist-header .content .playlist-image > img{
-  width: 100%;
-  height: 100%;
-}
-
-.playlist-header .content .playlist-info {
-  display: flex;
-  min-width: 600px;
-  height: 225px;
-  margin-left: 40px;
-  float: right;
-
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-
-  color: #8d8d8d;
-}
-
-.playlist-info .additional-info {
-  padding: 10px 0 0;
-}
-
-.playlist-info .additional-info li {
-  display: inline;
-  padding-left: 10px;
-  list-style: none;
-}
-
-.playlist-info .additional-info .playlist-author {
-  padding-left: 0;
-  font-weight: bold;
-  color: white;
-}
-
-.playlist-info .additional-info li::before {
-  content: '\2022';
-  margin-right: 10px;
-}
-
-.playlist-info .additional-info .playlist-author::before {
-  content: '';
-  margin-right: 0;
-}
-
-.playlist-info .playlist-description {
-  margin: 10px 0 0;
-}
-
-.playlist-info .playlist-title {
-  margin-bottom: 10px;
-  font-size: 48pt;
-  color: white;
-}
-
-.playlist-info .playlist-type {
-  margin-bottom: 30px;
-
-  font-weight: bold;
-  text-transform: uppercase;
-  color: white;
 }
 </style>
